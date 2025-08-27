b603a14e3b9b9562aa0686369d0463e4c190dfe4
Saikikunio <samuelpinheironogueira1pessoal@gmail.com>
2025-08-27 15:19:11 -0300

    login criado

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, jsonify
-
+from flask_login import LoginManager, login_user, current_user
 from database import db
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -16,9 +25,11 @@ def login():
 
     if username and password:
 
-        user = user.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação bem sucedida"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400
