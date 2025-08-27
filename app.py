053bd073b38cb08c30f6b8c70ff5cb557fb21b47
Saikikunio <samuelpinheironogueira1pessoal@gmail.com>
2025-08-27 16:15:31 -0300

    criacao de user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from database import db
 from models.user import User
 
@@ -34,7 +34,27 @@ def login():
 
     return jsonify({"message": "Credenciais inválidas"}), 400
 
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout bem-sucedido"})
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"message": "Usuario cadastrado com sucesso!"})
+    return jsonify({"message": "Dados inválidos!"}), 400
+
+    
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
